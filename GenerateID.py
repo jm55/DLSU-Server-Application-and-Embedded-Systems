f36a6e76ed01f8973fbf149ce234376b6f551b9d
Miguel Escalona - DLSU <71485171+jm55DLSU@users.noreply.github.com>
2023-07-16 10:40:56 +0800

    Added generators

--- GenerateID.py
+++ GenerateID.py
@@ -1,9 +1,9 @@
 import random
 
-rfid_tag_size = 1000
-
-with open('IDs.txt', 'a') as f:
+rfid_tag_size = int(input("Enter number of RFID tag nos. to generate: "))
+filename = input("Enter output filename: ")
+with open(filename, 'a') as f:
     for i in range(rfid_tag_size):
-        f.write("%032x" % random.getrandbits(128))
+        f.write("%032x\n" % random.getrandbits(128))
         f.flush()
     f.close()

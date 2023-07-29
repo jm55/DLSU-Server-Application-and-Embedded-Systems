842f4bd73b669f459707361350c6f882cf8aff96
Miguel Escalona - DLSU <71485171+jm55DLSU@users.noreply.github.com>
2023-07-30 00:25:59 +0800

    Updated ID Generators

--- JSON/GenerateDLSU.py
+++ JSON/GenerateDLSU.py
@@ -1,10 +1,15 @@
 import random
+import UI as ui
 
 dlsu_start = int(input("Enter starting DLSU ID no.: "))
 dlsu_ending = int(input("Enter ending DLSU ID no.: "))
+ctr = 0
 filename = input("Enter output filename: ")
 with open(filename, 'a') as f:
     for i in range(dlsu_start,dlsu_ending+1):
         f.write(f"{i}\n")
         f.flush()
+        ui.header("GenerateDLSU.py")
+        print(f"{(ctr/(dlsu_ending-dlsu_start))*100:4f}%")
+        ctr += 1
     f.close()
--- JSON/GenerateID.py
+++ JSON/GenerateID.py
@@ -1,4 +1,5 @@
 import random
+import UI as ui
 
 ids = []
 rfid_tag_size = int(input("Enter number of RFID tag nos. to generate: "))
@@ -14,5 +15,7 @@ with open(filename, 'w') as f:
                 f.write("%032x\n" % id) #results to 32 char hash
                 f.flush()
                 valid = True
+                ui.header("GenerateID.py")
+                print(f"{(i/rfid_tag_size)*100:4f}%")
     f.close()
 print(f"{rfid_tag_size} ID Nos. Generated!")

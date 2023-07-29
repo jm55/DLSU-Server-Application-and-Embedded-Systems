ce0f813bc635284f261e06762877afba8545e0b3
Miguel Escalona - DLSU <71485171+jm55DLSU@users.noreply.github.com>
2023-07-30 00:45:04 +0800

    Minor Updates
    1. Added 25K and 100K IDs
    2. Updated generators

--- JSON/GenerateDLSU.py
+++ JSON/GenerateDLSU.py
@@ -3,13 +3,18 @@ import UI as ui
 
 dlsu_start = int(input("Enter starting DLSU ID no.: "))
 dlsu_ending = int(input("Enter ending DLSU ID no.: "))
-ctr = 0
+if dlsu_ending < dlsu_start:
+    print("Correcting ending ID no...")
+    dlsu_ending *= 10
+    print(f"Set to {dlsu_ending}")
 filename = input("Enter output filename: ")
+writable = ""
 with open(filename, 'a') as f:
     for i in range(dlsu_start,dlsu_ending+1):
-        f.write(f"{i}\n")
-        f.flush()
-        ui.header("GenerateDLSU.py")
-        print(f"{(ctr/(dlsu_ending-dlsu_start))*100:4f}%")
-        ctr += 1
+        writable += f"{i}\n"
+    print("Writing to file...")
+    f.write(writable)
+    print("Flushing to file...")
+    f.flush()
     f.close()
+print(f"IDs {dlsu_start} to {dlsu_ending} ({dlsu_ending-dlsu_start} IDs) Generated!")
--- JSON/GenerateID.py
+++ JSON/GenerateID.py
@@ -5,6 +5,7 @@ ids = []
 rfid_tag_size = int(input("Enter number of RFID tag nos. to generate: "))
 filename = input("Enter output filename: ")
 print("Generating IDs...")
+writable = ""
 with open(filename, 'w') as f:
     for i in range(rfid_tag_size):
         valid = False
@@ -12,10 +13,11 @@ with open(filename, 'w') as f:
             id = random.getrandbits(128)
             if id not in ids:
                 ids.append(id)
-                f.write("%032x\n" % id) #results to 32 char hash
-                f.flush()
+                writable += "%032x\n" % id #results to 32 char hash
                 valid = True
-                ui.header("GenerateID.py")
-                print(f"{(i/rfid_tag_size)*100:4f}%")
+    print("Writing to file...")
+    f.write(writable)
+    print("Flushing to file...")
+    f.flush()
     f.close()
 print(f"{rfid_tag_size} ID Nos. Generated!")
